770f4db2a7ae9014ac0b001c7b479f9c5a9b93bd
samarjeet-singh-kheda <samarjeetsingh259@gmail.com>
2024-08-31 18:23:29 +0530

    REFACTOR: Star rating component made more reusable by defining user defined props, Added propTypes

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,23 +1,49 @@
 import { useState } from "react";
+import Prototypes from "prop-types"; // If we want to perform type-checking on the props, the we usually use typescript. But, this third party library can also be used, to do prop type-checking in JS.
 
 const containerStyle = { display: "flex", alignItems: "center", gap: "16px" };
 
 const starContainerStyle = { display: "flex" };
 
-const textStyle = {
-  lineHeight: "0",
+StarRating.propTypes = {
+  maxRating: Prototypes.number,
+  color: Prototypes.string,
+  size: Prototypes.string,
+  className: Prototypes.string,
+  messages: Prototypes.arrayOf(Prototypes.string),
+  defaultRating: Prototypes.number,
+  onSetRating: Prototypes.func,
 };
 
-export default function StarRating({ maxRating = 10 }) {
-  const [rating, setRating] = useState(0);
+export default function StarRating({
+  maxRating = 5,
+  color = "#fcc419",
+  size = "48",
+  className = "",
+  messages = [],
+  defaultRating = 0,
+  onSetRating,
+}) {
+  defaultRating =
+    defaultRating > 0 && defaultRating <= maxRating ? defaultRating : 0;
+
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
+    onSetRating?.(rating);
   }
 
+  const textStyle = {
+    lineHeight: "0",
+    margin: "0",
+    color,
+    fontSize: `${size / 1.5}px`,
+  };
+
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, idx) => (
           <Star
@@ -26,25 +52,29 @@ export default function StarRating({ maxRating = 10 }) {
             onRate={() => handleRating(idx + 1)}
             onHoverIn={() => setTempRating(idx + 1)}
             onHoverOut={() => setTempRating(0)}
+            color={color}
+            size={size}
           />
         ))}
       </div>
 
-      <p style={{ lineHeight: "1", margin: "0" }}>
-        {tempRating || rating || ""}
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
       </p>
     </div>
   );
 }
 
-const starStyle = {
-  width: "48px",
-  height: "48px",
-  display: "block",
-  cursor: "pointer",
-};
+function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    display: "block",
+    cursor: "pointer",
+  };
 
-function Star({ onRate, full, onHoverIn, onHoverOut }) {
   return (
     <span
       role="button"
@@ -57,8 +87,8 @@ function Star({ onRate, full, onHoverIn, onHoverOut }) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -67,7 +97,7 @@ function Star({ onRate, full, onHoverIn, onHoverOut }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./components/App";
-import StarRating from "./components/StarRating";
+import "./index.css";
+import App from "./components/App";
+
+// function Test() {
+//   const [rating, setRating] = useState(0);
+
+//   return (
+//     <div>
+//       <StarRating color="#2e46ff" maxRating={10} onSetRating={setRating} />
+//       <p>The movie was rated {rating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
-    <StarRating />
+    <App />
   </React.StrictMode>
 );
